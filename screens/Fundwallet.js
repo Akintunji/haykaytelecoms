2f1758dc7410e760f3555600b9ea5ab81a913e59
Wilmela <chukumelawilson@gmail.com>
2022-12-26 08:18:03 +0100

    --fixes

--- screens/Fundwallet.js
+++ screens/Fundwallet.js
@@ -1,150 +1,136 @@
-import React,{ useState,useEffect } from 'react';
-import { Text,TextInput, View,Button,Image,Alert,TouchableOpacity ,SafeAreaView,Keyboard ,ScrollView,StyleSheet } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  Button,
+  Image,
+  Alert,
+  TouchableOpacity,
+  SafeAreaView,
+  Keyboard,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-
-
-const fund =async (value) => {
-
-  fetch('http://127.0.0.1:8000/api/fundwallet', {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-     email : email,
-    amount:amount
+const fund = async (value) => {
+  fetch("http://127.0.0.1:8000/api/fundwallet", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      amount: amount,
+    }),
   })
-}) .then((response) => response.json())
+    .then((response) => response.json())
     .then((response) => {
-    if(response.status=='success'){
-        alert('wallet funded');
-    }      });
-
-  }
-
-
-
-export default function fundwallet() {
-  const [amount,set_amount]=useState('');
-  const [phone_number,set_Phonenumbe]=useState('');
-
-  const retrieveData = async () => {
-  try {
-    const email = await AsyncStorage.getItem('logged_in_email');
-
-   if (email.length == 0 || password.length==0) {
-      Alert('You are not loggd in');
-    navigation.navigate('Login')
-    }
-  } catch (error) {
-    // Error retrieving data
-  }
+      if (response.status == "success") {
+        alert("wallet funded");
+      }
+    });
 };
 
+export default function fundwallet() {
+  const [amount, set_amount] = useState("");
+  const [phone_number, set_Phonenumber] = useState("");
+
+  const retrieveData = async () => {
+    try {
+      const email = await AsyncStorage.getItem("logged_in_email");
+
+      if (email.length == 0 || password.length == 0) {
+        Alert("You are not loggd in");
+        navigation.navigate("Login");
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+  };
+
   return (
-<SafeAreaView style={styles.Safeareaview}>
- <ScrollView>
-<View style={styles.welcome}>
-      <Text>Reseller Ewallet</Text>
+    <SafeAreaView style={styles.Safeareaview}>
+      <ScrollView>
+        <View style={styles.welcome}>
+          <Text>Reseller Ewallet</Text>
+        </View>
 
-    </View>
- 
+        <View>
+          <Text style={styles.Buttons}>Enter Amount</Text>
 
+          <TextInput
+            style={styles.phoneinput}
+            placeholder="Enter Amount"
+            onChangeText={(Value) => set_amount(Value)}
+          />
 
+          <Text style={styles.Buttons}> Enter PhonesNumber </Text>
+          <TextInput
+            style={styles.phoneinput}
+            placeholder="Enter PhoneNumber"
+            onChangeText={(Value) => set_Phonenumber(Value)}
+          />
+        </View>
 
- <View>
-
- <Text style={styles.Buttons}>Enter Amount</Text>
-
- <TextInput
-        style={styles.phoneinput}
-        placeholder=" Enter Amount"
-        onChangeText={(Value) =>
-    set_amount(Value )
-  }/> 
-
-
-
-
- <Text  style={styles.Buttons}> Enter PhonesNumber </Text>
- <TextInput
-        style={styles.phoneinput}
-        placeholder=" Enter PhoneNumber"
-        onChangeText={(Value) =>
-    set_Phonenumber(Value )
-  }/> 
- </View>
-
-<Button title='Buy' style={{ width:300,}}/>
- 
-
-
-     
-
-</ScrollView>
-  </SafeAreaView>
-
+        <Button title="Buy" style={{ width: 300 }} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-header: {
-     flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
-fontSize:20,
-fontWeight:'bold',
-    height:80, 
-color: 'white',
-PaddingTop:38,
-  },
-title:{fontSize:20,
-fontWeight:'bold',
-color: 'white',},
- content: {
+  header: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-      borderColor: 'purple',
-      color:'purple',
-    
-  },  
-container: {
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    height: 80,
+    color: "white",
+    PaddingTop: 38,
+  },
+  title: { fontSize: 20, fontWeight: "bold", color: "white" },
+  content: {
     flex: 1,
-   
-    alignItems: 'center',
-    justifyContent: 'center',
-      borderColor: 'purple',
-      color:'white',
-   
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "purple",
+    color: "purple",
   },
-
-Buttons:{
-   PaddingTop:38,Top:20,
-    color: 'purple',
-    alignItems: 'center',
-
-fontWeight:'bold',
-  },
-
-
-footer: {
+  container: {
     flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
- Color: 'white',
- color: "#20232a",
- top: 270,
-height: 658,
+
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "purple",
+    color: "white",
   },
- image: {
+
+  Buttons: {
+    PaddingTop: 38,
+    Top: 20,
+    color: "purple",
+    alignItems: "center",
+
+    fontWeight: "bold",
+  },
+
+  footer: {
+    flex: 1,
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+    Color: "white",
+    color: "#20232a",
+    top: 270,
+    height: 658,
+  },
+  image: {
     width: 66,
     height: 58,
   },
-
 });
--- wallet/dashboard.js
+++ wallet/dashboard.js
@@ -1,203 +1,246 @@
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Modal,
+  View,
+  SafeAreaView,
+  Button,
+  ScrollView,
+  Alert,
+  Image,
+  Keyboard,
+  pressable,
+} from "react-native";
+import { Dimensions } from "react-native";
 
-import React, { useState,useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text,TouchableOpacity, Modal, View,SafeAreaView,Button,ScrollView,Alert,Image,Keyboard,pressable } from 'react-native';
-import { Dimensions } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import Vtu from '../wallet/vtu';
-import Cable from '../wallet/cable';
-import Data from '../wallet/data';
-import  Electricity from '../wallet/electricity';
-import Fundwallet from '../wallet/Fundwallet';
-
+import Vtu from "../wallet/vtu";
+import Cable from "../wallet/cable";
+import Data from "../wallet/data";
+import Electricity from "../wallet/electricity";
+import Fundwallet from "../wallet/Fundwallet";
 
 function Child_dashboard({ navigation }) {
-  const [balance, setbalance]= useState('');
-  const [token, settoken]= useState('');
-  const [email, setemail]= useState('');
+  const [balance, setbalance] = useState("");
+  const [token, settoken] = useState("");
+  const [email, setemail] = useState("");
 
+  async function retrieveUserSession() {
+    try {
+      const session = await AsyncStorage.getItem("user_session");
 
-async function retrieveUserSession() {
-  try {   
-      const session=await AsyncStorage.getItem("user_session");
-
-    if (session!==undefined) {
-      const session_items=JSON.parse(session);
-  setemail(session_items.email);
-  settoken(session_items.token);
-  setbalance(session_items.balance);
-
+      if (session !== undefined) {
+        const session_items = JSON.parse(session);
+        setemail(session_items.email);
+        settoken(session_items.token);
+        setbalance(session_items.balance);
+      } else {
+        console.log("Empty values");
       }
-      else{console.log('Empty values')}
-  } catch (error) {
-      
+    } catch (error) {}
   }
+
+  useEffect(() => {
+    retrieveUserSession();
+  });
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={styles.Text}>Your Wallet balance is</Text>
+        <Text>Welcome {email}</Text>
+      </View>
+      <View style={{ marginTop: 50 }} />
+
+      <View style={styles.pcontainer}>
+        <View style={styles.container}>
+          <View>
+            <Image style={styles.image} source={require("../assets/vtu.jpg")} />
+            <TouchableOpacity onPress={() => navigation.navigate("Vtu")}>
+              {" "}
+              <Text style={styles.Text}>Buy Vtu</Text>{" "}
+            </TouchableOpacity>
+          </View>
+
+          <View>
+            <Image
+              style={styles.image}
+              source={require("../assets/data.jpg")}
+            />
+            <TouchableOpacity onPress={() => navigation.navigate("Data")}>
+              {" "}
+              <Text style={styles.Text}>Buy Data </Text>
+            </TouchableOpacity>{" "}
+          </View>
+          <View>
+            <Image
+              style={styles.image}
+              source={require("../assets/home2.jpg")}
+            />
+            <TouchableOpacity onPress={() => navigation.navigate("transfer")}>
+              {" "}
+              <Text style={styles.Text}>Transfer</Text>{" "}
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.container}>
+          <View>
+            <Image
+              style={styles.image}
+              source={require("../assets/cable.jpg")}
+            />
+            <TouchableOpacity onPress={() => navigation.navigate("Cable")}>
+              <Text style={styles.Text}> Cable TV</Text>
+            </TouchableOpacity>{" "}
+          </View>
+
+          <View>
+            <Image
+              style={styles.image}
+              source={require("../assets/electricity.jpg")}
+            />{" "}
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Electricity")}
+            >
+              <Text style={styles.Text}>Buy Electricity </Text>
+            </TouchableOpacity>{" "}
+          </View>
+          <View>
+            <Image style={styles.image} source={require("../assets/vtu.jpg")} />
+            <TouchableOpacity onPress={() => navigation.navigate("vtu")}>
+              {" "}
+              <Text style={styles.Text}>Fund Wallet</Text>{" "}
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
- 
-
-useEffect(() => {
-  retrieveUserSession();
- });
-
-
-
-
-
-
-
-return(
-<SafeAreaView>
-
-
-
-<View><Text style={styles.Text}>Your Wallet balance is</Text>
-<Text>Welcome {email}</Text> 
-</View>
-<View style={{marginTop:50 }}/>
-
- 
- <View style={styles.pcontainer}>
- <View style={styles.container}>
-
- <View ><Image  style={styles.image} source={require("../assets/vtu.jpg")}/>
- <TouchableOpacity onPress={() => navigation.navigate('Vtu')}> <Text  style={styles.Text}>Buy Vtu</Text> </TouchableOpacity ></View>
-
- <View ><Image  style={styles.image} source={require("../assets/data.jpg")}/><TouchableOpacity onPress={() => navigation.navigate('Data')}>  <Text  style={styles.Text}>Buy Data </Text></TouchableOpacity > </View>
- <View ><Image  style={styles.image} source={require("../assets/home2.jpg")}/><TouchableOpacity onPress={() => navigation.navigate('transfer')}> <Text  style={styles.Text}>Transfer</Text> </TouchableOpacity ></View>
-
-</View>
-
- <View style={styles.container}>
-
-   <View><Image  style={styles.image} source={require("../assets/cable.jpg")}/><TouchableOpacity onPress={() => navigation.navigate('Cable')}><Text  style={styles.Text}> Cable TV</Text></TouchableOpacity > </View>
-  
- <View><Image  style={styles.image} source={require("../assets/electricity.jpg")}/> <TouchableOpacity onPress={() => navigation.navigate('Electricity')}><Text  style={styles.Text}>Buy Electricity </Text></TouchableOpacity > </View>
-<View ><Image  style={styles.image} source={require("../assets/vtu.jpg")}/><TouchableOpacity onPress={() => navigation.navigate('vtu')}> <Text  style={styles.Text}>Fund Wallet</Text> </TouchableOpacity ></View>
-
-
-
- </View>
-   </View>
- </SafeAreaView> 
-);
-}
-
-
 
 const styles = StyleSheet.create({
-header: {
-     flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
-fontSize:20,
-fontWeight:'bold',
-    height:80, 
-color: 'white',
-PaddingTop:38,
+  header: {
+    flex: 1,
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    height: 80,
+    color: "white",
+    PaddingTop: 38,
   },
-title:{fontSize:20,
-fontWeight:'bold',
-color: 'white',},
- content: {
+  title: { fontSize: 20, fontWeight: "bold", color: "white" },
+  content: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-      borderColor: 'purple',
-      color:'purple',
-    
-  }, 
-  Text:{color:'blue',}, 
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "purple",
+    color: "purple",
+  },
+  Text: { color: "blue" },
 
-pcontainer: {
+  pcontainer: {
     flex: 1,
-    justifyContent: 'center',
-      borderColor: 'white',
-      color:'white',
-   
+    justifyContent: "center",
+    borderColor: "white",
+    color: "white",
   },
 
-
-container: {
+  container: {
     flex: 1,
-top:170,
-    justifyContent: 'center',
-      borderColor: 'white',
-      color:'white',
-   flexDirection:'row',
+    top: 170,
+    justifyContent: "center",
+    borderColor: "white",
+    color: "white",
+    flexDirection: "row",
   },
 
-Buttons:{
-   PaddingTop:38,Top:20,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-Color: '#fff',
- width: 96,
+  Buttons: {
+    PaddingTop: 38,
+    Top: 20,
+    backgroundColor: "purple",
+    alignItems: "center",
+    Color: "#fff",
+    width: 96,
     height: 98,
-fontWeight:'bold',
+    fontWeight: "bold",
   },
 
-
-footer: {
+  footer: {
     flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'center',
- Color: 'white',
- color: "#20232a",
- top: 270,
-height: 658,
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+    Color: "white",
+    color: "#20232a",
+    top: 270,
+    height: 658,
   },
- image: {
+  image: {
     width: 66,
     height: 88,
-    Top:20,
+    Top: 20,
   },
-
 });
 const Tab = createBottomTabNavigator();
 
 export default function Dashboard() {
+  return (
+    <NavigationContainer independent="true">
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            position: "absolute",
+            fontWeight: "bold",
+            backgroundColor: "#000080",
+            color: "gold",
+            showIcon: false,
+          },
+          tabBarIconStyle: { display: "none" },
+          tabBarLabelStyle: { marginVertical: 15 },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Child_dashboard}
+          options={{ headerShown: false }}
+        />
 
-  return (  
-    <NavigationContainer independent='true'>
-   <Tab.Navigator  screenOptions={{ tabBarStyle: { position: 'absolute',fontWeight:'bold',backgroundColor: '#000080',color:'gold', showIcon:false, },
-    tabBarIconStyle: {display:"none", },
-tabBarLabelStyle:{marginVertical:15}
-
-  }}>
-   <Tab.Screen name="Home" component={Child_dashboard}
-              options={{ headerShown: false,
-               
-              }}
-              />
-           
-           <Tab.Screen name="Vtu" component={Vtu}
-              options={{ headerShown: false,
-              }}
-              />
-         <Tab.Screen name="Data" component={Data}
-              options={{ headerShown: false }}
-              />
-               <Tab.Screen name="Cable" component={Cable}
-              options={{ headerShown: false }}
-              />
-                     <Tab.Screen name="Electricity" component={Electricity}
-              options={{ headerShown: false }}
-              />
-                       <Tab.Screen name="transfer" component={Transfer}
-              options={{ headerShown: false }}
-              />
-   </Tab.Navigator >
-   
-
-</NavigationContainer>
-
+        <Tab.Screen
+          name="Vtu"
+          component={Vtu}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Data"
+          component={Data}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Cable"
+          component={Cable}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Electricity"
+          component={Electricity}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="transfer"
+          component={Transfer}
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-
 }
